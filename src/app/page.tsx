b897d96fa053ab09f96e2eac2cffd0d56bbeadeb
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2024-02-21 17:09:05 -0500

    fix: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
-import { GroupCreation } from "./group-creation";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
@@ -8,16 +8,12 @@ export default function HomePage() {
       <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16 '>
         <h1 className='text-5xl font-extrabold tracking-tight sm:text-[5rem]'>Church Life</h1>
 
-        <div className=''>
-          <GroupCreation />
-          <SignedIn>
-            <UserButton />
-            <SignOutButton />
-          </SignedIn>
+        <div className='flex flex-col items-center'>
+          <p className='text-lg font-semibold'>Haga click en el botón para ingresar al dashboard</p>
 
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
+          <Button className='mx-auto' asChild>
+            <Link href='/dashboard'>Dashboard</Link>
+          </Button>
         </div>
       </div>
     </main>
